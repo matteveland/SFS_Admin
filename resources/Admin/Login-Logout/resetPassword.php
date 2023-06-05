9d053adb48fa1f074fffd1c254ebbbab2849ae43
matteveland <matt.eveland@gmail.com>
2023-06-05 10:36:40 -0700

    removed old files

--- resources/Admin/Login-Logout/resetPassword.php
+++ resources/Admin/Login-Logout/resetPassword.php
@@ -1,16 +1,16 @@
 <?php
-include $_SERVER['DOCUMENT_ROOT'].'/config/connect.php';
-require ('/Users/matteveland/code/data.env');
+include $_SERVER['DOCUMENT_ROOT'] . '/config/connect.php';
+//require ('/Users/matteveland/code/data.env');
 if (isset($_POST['submit'])) {
 
-   // $email = ($_POST['email']);
-  //  $encodedEmail = urlencode($email);
-  //  $email = $mysqli->real_escape_string($encodedEmail);
+    // $email = ($_POST['email']);
+    //  $encodedEmail = urlencode($email);
+    //  $email = $mysqli->real_escape_string($encodedEmail);
     $userName = $mysqli->real_escape_string($_POST['userName']);
     $last = $mysqli->real_escape_string($_POST['lastName']);
     $first = $mysqli->real_escape_string($_POST['firstName']);
     $middle = $mysqli->real_escape_string($_POST['middleName']);
-   // $dodId = $mysqli->real_escape_string($_POST['dodId']);
+    // $dodId = $mysqli->real_escape_string($_POST['dodId']);
     $TAFMSD = $mysqli->real_escape_string($_POST['TAFMSD']);
     //$unitName = $mysqli->real_escape_string($_POST['unitNameDropDown']);
     $secretQuestion = $mysqli->real_escape_string($_POST['secretQuestion']);
@@ -20,25 +20,24 @@ if (isset($_POST['submit'])) {
     $newpassword = $mysqli->real_escape_string($_POST['password']);
     $passwordVerify = $mysqli->real_escape_string($_POST['newPasswordVerify']);
 
-    $queryAllTrue = $mysqli->query("SELECT * FROM `login` WHERE (user_name='$userName' AND lastName = '$last' AND firstName = '$first' AND middleName = '$middle' AND enterDate = '$TAFMSD')") or die (mysqli_errno($mysqli));
+    $queryAllTrue = $mysqli->query("SELECT * FROM `login` WHERE (user_name='$userName' AND lastName = '$last' AND firstName = '$first' AND middleName = '$middle' AND enterDate = '$TAFMSD')") or die(mysqli_errno($mysqli));
 
     //$resultQueryAllTrue = mysqli_query($connection, $queryAllTrue) or die(mysqli_error($connection));
 
     while ($row = $queryAllTrue->fetch_assoc()) {
 
         $secretResults = $row['secret'];
-       // $emailResults = $row['email'];
+        // $emailResults = $row['email'];
 
         $secretResultsQuestion = $secretResults[0];
         $secretResultsAnswer = $secretResults[1];
-
     }
 
     //$iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length($cipherMethod));
-  //  $encryptedEmail = openssl_encrypt($email, $cipherMethod, $emailKey, $options=0, $iv);
+    //  $encryptedEmail = openssl_encrypt($email, $cipherMethod, $emailKey, $options=0, $iv);
 
 
-   // $emailDecrypted = openssl_decrypt($emailResults, $cipherMethod, $emailKey, 0, $iv);
+    // $emailDecrypted = openssl_decrypt($emailResults, $cipherMethod, $emailKey, 0, $iv);
 
     if (($secretArray == $secretResults)) {
 
@@ -53,7 +52,6 @@ if (isset($_POST['submit'])) {
                 //$resultUpdatePassword = mysqli_query($connection, $updatePassword) or die(mysqli_error($connection));
 
                 $successmsg = "Password was successfully updated/changed.";
-
             } else {
                 $errorMessage = "The information supplied does not match the records in the database.";
             }
@@ -67,37 +65,28 @@ if (isset($_POST['submit'])) {
             if ($secretResultsQuestion == '1') {
 
                 $recoveryQuestionMismatch = "Your recovery question does not match what you supplied when registering your account.<br> Please select: What is your favorite color? and try again. ";
-
             } elseif ($secretResultsQuestion == '2') {
                 $recoveryQuestionMismatch = "Your recovery question does not match what you supplied when registering your account.<br> Please select: Make of your first car? and try again. ";
-
             } elseif ($secretResultsQuestion == '3') {
                 $recoveryQuestionMismatch = "Your recovery question does not match what you supplied when registering your account.<br> Please select: Favorite sport? and try again. ";
-
             } elseif ($secretResultsQuestion == '4') {
                 $recoveryQuestionMismatch = "Your recovery question does not match what you supplied when registering your account.<br> Please select: Best location your have visited? and try again. ";
-
             } elseif ($secretResultsQuestion == '5') {
                 $recoveryQuestionMismatch = "Your recovery question does not match what you supplied when registering your account.<br> Please select: Are sharks fish? and try again. ";
-
-
             } else {
 
                 $failuremsg = "Your personal information does not match the database. Please contact system admin 1";
-
             }
         } else {
             $failuremsg = "You have no recovery question. Please contact system admin 2";
-
         }
-
     }
-
 }
 
 ?>
 
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -107,39 +96,41 @@ if (isset($_POST['submit'])) {
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script>
-        $(document).ready(function(){
+        $(document).ready(function() {
             $("#myModal").modal('show');
         });
     </script>
     <style>
-        .bs-example{
+        .bs-example {
             margin: 20px;
         }
     </style>
 </head>
+
 <body>
 
-<div class="bs-example">
-    <div id="myModal" class="modal fade" tabindex="-1">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Logging In</h5>
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                </div>
-                <div class="modal-body">
+    <div class="bs-example">
+        <div id="myModal" class="modal fade" tabindex="-1">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Logging In</h5>
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+                    <div class="modal-body">
 
-<?php if(isset($successmsg)){ ?><div class="alert alert-success" role="alert" style="text-align: center"> <?php echo $successmsg; ?> </div><?php } ?>
-<?php if(isset($failuremsg)){ ?><div class="alert alert-danger" role="alert" style="text-align: center"> <?php echo $failuremsg; ?> </div><?php } ?>
-<?php if(isset($errorMessage)){ ?><div class="alert alert-danger" role="alert" style="text-align: center"> <?php echo $errorMessage; ?> </div><?php } ?>
-<?php if(isset($recoveryQuestionMismatch)){ ?><div class="alert alert-secondary" role="alert" style="text-align: center"> <?php echo $recoveryQuestionMismatch; ?> </div><?php } ?>
+                        <?php if (isset($successmsg)) { ?><div class="alert alert-success" role="alert" style="text-align: center"> <?php echo $successmsg; ?> </div><?php } ?>
+                        <?php if (isset($failuremsg)) { ?><div class="alert alert-danger" role="alert" style="text-align: center"> <?php echo $failuremsg; ?> </div><?php } ?>
+                        <?php if (isset($errorMessage)) { ?><div class="alert alert-danger" role="alert" style="text-align: center"> <?php echo $errorMessage; ?> </div><?php } ?>
+                        <?php if (isset($recoveryQuestionMismatch)) { ?><div class="alert alert-secondary" role="alert" style="text-align: center"> <?php echo $recoveryQuestionMismatch; ?> </div><?php } ?>
 
-                    <p style="text-align: center">Redirecting to login page</p>
-                    <?php  header("refresh:2;url=login.php");?>
+                        <p style="text-align: center">Redirecting to login page</p>
+                        <?php header("refresh:2;url=login.php"); ?>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 </body>
+
 </html>
--- resources/Admin/Login-Logout/x_verifyLogin.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
-if (!isset($_SESSION['page_admin']) && (!isset($_SESSION['page_user']))) {
-    //Does not exist. Redirect user back to page-one.php
-    header("location:/resources/Admin/Login-Logout/login.php");
-
-    exit;
-  }
